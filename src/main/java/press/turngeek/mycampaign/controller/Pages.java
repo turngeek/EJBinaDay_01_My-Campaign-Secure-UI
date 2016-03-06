08a7039abd46d87071e9a2f07b735c4905188ab1
Anonymous <anonymous@noemail.com>
2016-03-06 11:45:16 +0000

    init

---
+++ src/main/java/press/turngeek/mycampaign/controller/Pages.java
@@ -0,0 +1,9 @@
+package press.turngeek.mycampaign.controller;
+
+public class Pages {
+	public static final String LIST_CAMPAIGNS = "/organizer/listCampaigns";
+	public static final String EDIT_CAMPAIGN = "/organizer/listCampaignseditCampaign";
+	public static final String EDIT_DONATION_FORM = "/organizer/listCampaignseditDonationForm";
+	public static final String LIST_DONATIONS = "/organizer/listCampaignslistDonations";
+	public static final String DONATE_MONEY = "donateMoney";
+}
